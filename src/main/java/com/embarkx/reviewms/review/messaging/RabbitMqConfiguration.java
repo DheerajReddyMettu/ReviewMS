2bc5dd0d6792caf9790989cfd5bdd8b93940c771
mettudheerajreddy67@gmail.com <mettuDheerajReddy67@gmail.com>
2024-06-18 05:59:51 -0400

    Add Project ReviewMs

---
+++ src/main/java/com/embarkx/reviewms/review/messaging/RabbitMqConfiguration.java
@@ -0,0 +1,30 @@
+package com.embarkx.reviewms.review.messaging;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+
+@Configurable
+public class RabbitMqConfiguration {
+
+    @Bean
+    public Queue companyRatingQueue(){
+        return new Queue("companyRatingQueue");
+    }
+
+    @Bean
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+}
